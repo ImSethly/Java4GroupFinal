41edcfbe9bbbed825c0b895fe7ba2403989b1c4c
Ayoubbenkhedher <ayoub.benkhedher@igentax.com>
2022-04-09 05:22:39 -0500

    created ItemHolder and ItemList classes

--- src/Item.java
+++ src/Item.java
@@ -1,9 +1,31 @@
 public class Item {
 
-    String name;
+    private String name;
+    private String description;
 
-    public Item(String name) {
+    //Item constructor
+    public Item(String name, String aDescription) {
         this.name = name;
+        this.description = aDescription;
     }
 
+    //get item name
+    public String getName() {
+        return name;
+    }
+
+    //set item name
+    public void setName(String aName) {
+        this.name = aName;
+    }
+
+    //get item description
+    public String getDescription() {
+        return description;
+    }
+
+    //set item description
+    public void setDescription(String aDescription) {
+        this.description = aDescription;
+    }
 }
---
+++ src/ItemHolder.java
@@ -0,0 +1,23 @@
+
+
+
+public class ItemHolder extends Item implements java.io.Serializable {
+
+    private ItemList items = new ItemList();
+
+    //constructor for ItemHolder
+    public ItemHolder(String aName, String aDescription, ItemList tl) {
+        super(aName, aDescription);
+        items = tl;
+    }
+
+    // return the list of items in the ItemHolder
+    public ItemList getItems() {
+        return items;
+    }
+
+    //set a list of items in the item holder
+    public void setItems(ItemList things) {
+        this.items = items;
+    }
+}
